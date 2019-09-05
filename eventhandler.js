6ade7978e9ee5b110f2ced51afe65c9290fbf770
lanserdi <lanserdi@163.com>
2019-09-05 21:33:23 +0800

    update

--- eventhandler.js
+++ eventhandler.js
@@ -1,10 +1,10 @@
 import model from "./model";
 import * as view from "./view";
 
-let hooks = {};
+let HOOKS = {};
 
-export let registEventHandler = (handler) => {
-  hooks = handler;
+export let registEventHandler = (customHooks) => {
+  HOOKS = customHooks;
 
   view.els.appEl.addEventListener("dragover", appEl_onDragOver);
   view.els.appEl.addEventListener("dragleave", appEl_onDragLeave);
@@ -18,18 +18,18 @@ export let registEventHandler = (handler) => {
   window.addEventListener("keydown", window__onKeyDown);
 };
 
-let statusEl__onClick = function(e){
+let statusEl__onClick = function(e) {
   let target = e.target;
-  while(this != target){
+  while (this != target) {
     let feat = target.dataset && target.dataset.feat
-    if(feat){
-      switch(feat){
+    if (feat) {
+      switch (feat) {
         case "getLabel":
-          if(typeof hooks.getLabel === "function"){
-            hooks.getLabel(model.activeShape);
+          if (typeof HOOKS.getLabel === "function") {
+            HOOKS.getLabel(model.activeShape);
           }
         default:
-        break;
+          break;
       }
     }
     target = target.parentNode;
@@ -39,7 +39,7 @@ let statusEl__onClick = function(e){
 let appEl_onDrop = e => {
   e.preventDefault();
   let file = e.dataTransfer.files[0]
-  if(!file || file.type.indexOf("image") === -1) return;
+  if (!file || file.type.indexOf("image") === -1) return;
   view.els.appEl.classList.add("GOMA_LABEL");
   let reader = new FileReader();
   reader.onloadend = e => {
@@ -62,12 +62,15 @@ let appEl_onDragOver = e => {
 
 let canvasWrapEl__onMouseDown = e => {
   e.preventDefault();
-  let point = { x: e.offsetX, y: e.offsetY };
+  let point = {
+    x: e.offsetX,
+    y: e.offsetY
+  };
   model.isMouseDown = true;
-  if(e.button == 0){
-    if(!model.isDrawing){
+  if (e.button == 0) {
+    if (!model.isDrawing) {
       view.startDraw(point);
-    }else{
+    } else {
       view.addPoint(point);
     }
   }
@@ -75,33 +78,36 @@ let canvasWrapEl__onMouseDown = e => {
 
 let canvasWrapEl__onMouseMove = e => {
   e.preventDefault();
-  let point = { x: e.offsetX, y: e.offsetY };
-  if(model.isDrawing){
+  let point = {
+    x: e.offsetX,
+    y: e.offsetY
+  };
+  if (model.isDrawing) {
     var labelPoints = model.currentLabel.points;
-    labelPoints[labelPoints.length-1] = [point.x, point.y];
+    labelPoints[labelPoints.length - 1] = [point.x, point.y];
     view.renderLabelCanvas();
   }
 };
-  
+
 let canvasWrapEl__onMouseUp = e => {
   e.preventDefault();
   model.isMouseDown = true
 };
- 
+
 let canvasWrapEl__onContextMenu = e => {
   e.preventDefault();
-  if(e.button == 2) view.stopDraw();
+  if (e.button == 2) view.stopDraw();
 };
 
 // labelImagesWrapEl
 let labelImagesWrapEl__onClick = e => {
   let target = e.target;
   let feats = {
-    deleteLabel: function(e, target){
+    deleteLabel: function(e, target) {
       let labelImageEl = target.parentNode;
       let labelTimestamp = labelImageEl.dataset.labelTimestamp;
-      model.labelInfo.shapes.forEach(function(label, index){
-        if(label.timestamp == labelTimestamp){
+      model.labelInfo.shapes.forEach(function(label, index) {
+        if (label.timestamp == labelTimestamp) {
           model.labelInfo.shapes.splice(index, 1);
           labelImageEl.parentNode.removeChild(labelImageEl);
           view.renderLabelCanvas();
@@ -110,12 +116,12 @@ let labelImagesWrapEl__onClick = e => {
       view.els.statusEl.innerText = "";
       return "stop";
     },
-    selectLabel: function(e, target){
+    selectLabel: function(e, target) {
       view.unSeelctLabel();
       let labelTimestamp = target.dataset.labelTimestamp;
       target.classList.add("active");
-      model.labelInfo.shapes.forEach(function(label, index){
-        if(label.timestamp == labelTimestamp){
+      model.labelInfo.shapes.forEach(function(label, index) {
+        if (label.timestamp == labelTimestamp) {
           model.activeShape = label;
           view.renderLabelCanvas();
         }
@@ -123,16 +129,16 @@ let labelImagesWrapEl__onClick = e => {
       let active = model.activeShape
       view.els.statusEl.innerHTML = `<span>[${active.name||"未命名标记"}]  宽=${active.width}像素  高=${active.height}像素  距左=${active.left}像素  距顶=${active.top}像素  </span><a href="${active.imageData}" class="download" download="${active.name||"未命名标记"}.png">下载图片</a> <a data-feat="getLabel" href="javascript:;">获取标注数据</a>`;
     },
-    showInput: function(e, target){
+    showInput: function(e, target) {
       view.els.statusEl.innerText = "";
       let input = window.prompt("输入标注内容：", target.innerText);
-      if(!input) return;
-      input = input.replace(/^\s+|\s$/g,"");
+      if (!input) return;
+      input = input.replace(/^\s+|\s$/g, "");
       input = input.replace(/</g, "&lt;");
       let labelImageEl = target.parentNode;
       let labelTimestamp = labelImageEl.dataset.labelTimestamp;
-      model.labelInfo.shapes.forEach(function(label, index){
-        if(label.timestamp == labelTimestamp){
+      model.labelInfo.shapes.forEach(function(label, index) {
+        if (label.timestamp == labelTimestamp) {
           label.name = input;
           target.innerText = input;
         }
@@ -140,24 +146,24 @@ let labelImagesWrapEl__onClick = e => {
       return "stop";
     }
   }
-  while(target !== this){
-    if(!target) return;
+  while (target !== this) {
+    if (!target) return;
     let feat = target.dataset && target.dataset.feat;
     let go = feat && feats.hasOwnProperty(feat) && feats[feat](e, target);
-    if(go === "stop") return;
+    if (go === "stop") return;
     target = target.parentNode;
   }
 };
 
 let window__onKeyDown = e => {
   var currKey = e.keyCode || e.which || e.charCode
-  // key -> q
-  if(currKey == 81){
+    // key -> q
+  if (currKey == 81) {
     e.preventDefault();
     view.stopDraw();
   }
   // key -> ctrl + z
-  if(e.ctrlKey || e.metaKey && currKey == 90){
+  if (e.ctrlKey || e.metaKey && currKey == 90) {
     e.preventDefault();
     view.unDoDraw();
   }
--- index.js
+++ index.js
@@ -4,10 +4,14 @@ import model from "./model";
 import * as utils from "./utils";
 import * as eventHandler from "./eventhandler";
 
-let label = (appEl, hooks) => {
-  view.insertEls(appEl);
-  eventHandler.registEventHandler(hooks || {});
-  return Object.assign({}, utils);
+let label = (appEl, opts) => {
+  return Object.assign(() => {
+    view.insertEls(appEl);
+    // 注册事件
+    eventHandler.registEventHandler(opts && opts.hooks || {});
+  }, utils);
 };
 
-export { label };
+export {
+  label
+};
